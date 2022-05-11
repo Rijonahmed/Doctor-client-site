e1888ea511d01e0fc94edcfcff7239d127be4537
Rijonahmed <rijon036@gmail.com>
2022-05-12 01:32:10 +0600

    services, exceptional, make appoentment section add

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import Banner from './Banner';
+import Exceptional from './Exceptional';
 import Info from './Info';
+import MakeAppoentment from './MakeAppoentment';
+import Services from './Services';
 
 const Home = () => {
   return (
-    <div>
+    <div className='mx-12'>
       <Banner></Banner>
       <Info></Info>
+      <Services></Services>
+      <Exceptional></Exceptional>
+      <MakeAppoentment></MakeAppoentment>
 
     </div>
   );
--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -6,10 +6,10 @@ import phone from '../../assets/icons/phone.svg'
 
 const Info = () => {
   return (
-    <div className='grid grid-cols-1 lg:grid-cols-3 gap-5'>
-      <InfoCard img={clock}></InfoCard>
-      <InfoCard img={marker}></InfoCard>
-      <InfoCard img={phone}></InfoCard>
+    <div className='grid grid-cols-1 lg:grid-cols-3 gap-5 px-12'>
+      <InfoCard bgClass='bg-gradient-to-r from-secondary to-primary' cardTitle='Opening Hours' img={clock}></InfoCard>
+      <InfoCard bgClass='bg-accent' cardTitle='Visit our location' img={marker}></InfoCard>
+      <InfoCard bgClass='bg-gradient-to-r from-secondary to-primary' cardTitle='Contact us now' img={phone}></InfoCard>
 
     </div>
   );
