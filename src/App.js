65d363ee8589af00b717ed1b97c3b1ccd32f5968
Rijonahmed <rijon036@gmail.com>
2022-05-11 22:01:55 +0600

    banner added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Pages/About/About';
+import Appointment from './Pages/Appointment/Appointment';
+import ContactUs from './Pages/ContactUs/ContactUs';
+import Home from './Pages/Home/Home';
+import LogIn from './Pages/LonIn/LogIn';
+import Reviews from './Pages/Reviews/Reviews';
+import Navber from './Pages/Shared/Navber/Navber';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <Navber></Navber>
+      <Routes>
+
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='/appointment' element={<Appointment></Appointment>}></Route>
+        <Route path='/reviews' element={<Reviews></Reviews>}></Route>
+        <Route path='/contactus' element={<ContactUs></ContactUs>}></Route>
+        <Route path='/login' element={<LogIn></LogIn>}></Route>
+
+      </Routes>
 
 
 
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
